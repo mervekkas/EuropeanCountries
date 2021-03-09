5a195728c646c444c8b4944fd02aab29169ada80
mervekkas <mervekkas@icloud.com>
2021-03-09 14:44:25 +0300

    first commit

---
+++ app/src/main/java/com/mvks/europeancountries/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.mvks.europeancountries.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mvks.europeancountries.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
